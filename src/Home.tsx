187df8dedf4027d3e90b87aaa7fae251da0f831f
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-09-11 01:56:03 +0300

    feat: add routing and helmeta to project

---
+++ src/Home.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+
+type Test = [
+  {
+    category: string;
+    description: string;
+  }
+];
+
+export const Home = () => {
+  const [testMeta, setTestMeta] = useState<Test | null>(null);
+
+  const fetchTestData = () => {
+    fetch(
+      "http://ec2-3-120-174-49.eu-central-1.compute.amazonaws.com:7000/dev/test"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTestMeta(data);
+      });
+  };
+
+  console.log("testMeta", testMeta);
+
+  useEffect(() => {
+    fetchTestData();
+  }, []);
+  return (
+    <div>
+      {testMeta && (
+        <Helmet
+          title={testMeta[0].category}
+          meta={[
+            { property: "og:type", content: "website" },
+            { property: "og:title", content: testMeta[0].category },
+            { property: "og:url", content: "https://test.ru/" },
+            {
+              property: "og:image",
+              content:
+                "https://hsto.org/r/w1560/webt/dd/bj/ed/ddbjedwsswlaapzkfxs33ddfflw.png",
+            },
+            {
+              property: "og:description",
+              content: testMeta[0].description,
+            },
+          ]}
+        />
+      )}
+      <h1>HOME</h1>
+    </div>
+  );
+};
---
+++ src/Market.tsx
@@ -0,0 +1,26 @@
+import { Helmet } from "react-helmet";
+
+export const Market = () => {
+  return (
+    <div>
+      <Helmet
+        title="MARKET"
+        meta={[
+          { property: "og:type", content: "website" },
+          { property: "og:title", content: "Страница Market нашего Dobre" },
+          { property: "og:url", content: "https://test.com/" },
+          {
+            property: "og:image",
+            content:
+              "https://blog.promopult.ru/wp-content/uploads/2019/02/checklist01-1200x630.png",
+          },
+          {
+            property: "og:description",
+            content: "Тестовое описание страницы Market нашего Dobre",
+          },
+        ]}
+      />
+      <h1>MARKET</h1>
+    </div>
+  );
+};
