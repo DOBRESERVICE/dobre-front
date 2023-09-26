b05eb42a1f312437ddef20b7c011d567123b1020
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-09-26 22:24:38 +0300

    refactor:folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,34 @@
 "use client";
-import { MainLayout } from "@/components/MainLayout";
-import DOBRE from "@/assets/image/DOBRE.svg";
-import Image from "next/image";
-import { LogInComponent } from "@/components/AuthorizationComponents/LogInComponent";
-import autorizBanner from "@/assets/image/leftBanner.png";
-import { CreateAccountComponent } from "@/components/AuthorizationComponents/CreateAccountComponent";
-import { useState } from "react";
-import { ResetPasswordComponent } from "@/components/AuthorizationComponents/ResetPasswordComponent";
-import whiteLogo from "@/assets/image/whiteLogo.svg";
+import { ModalComponent } from "@/components/ModalComponent";
+import { CreateAccountMess } from "@/components/NotificationMessageModal/CreateAccount";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import {HomeLayout} from "@/components/homeLayout/HomeLayout";
 
-export default function Autorization() {
-  const [activeStep, setActiveStep] = useState("create");
+export default function HomePage() {
+    const currentUrl = window.location;
+    const router = useRouter();
+    const [showModal, setIsShowModal] = useState<boolean>();
 
-  return (
-    <MainLayout>
-      <div className="autorizationWrapper">
-        <div className="componentWrapper">
-          <div
-            className="leftBanner"
-            style={{ backgroundImage: `url(${autorizBanner.src}` }}
-          >
-            <div className="leftBannerTitle">
-              <img alt="logo" src={whiteLogo.src} />
-            </div>
-            <div className="leftBannerDescr">
-              Легко арендовать и безопасно сдавать
-            </div>
-          </div>
-          {activeStep === "login" && (
-            <LogInComponent setActiveStep={setActiveStep} />
-          )}
-          {activeStep === "create" && (
-            <CreateAccountComponent setActiveStep={setActiveStep} />
-          )}
-          {activeStep === "reset" && <ResetPasswordComponent />}
-        </div>
+    useEffect(() => {
+        setIsShowModal(!!currentUrl?.search.split("=")[1]);
+    }, [currentUrl]);
 
-        <Image src={DOBRE} alt="DOBRE" className="dobreImage" />
-      </div>
-    </MainLayout>
-  );
+    return (
+     <HomeLayout>
+         <ModalComponent
+             open={showModal}
+             handleClose={() => {
+                 router.push("/home");
+                 setIsShowModal(false);
+             }}
+         >
+             <CreateAccountMess />
+             {/* <LinkResetMess /> */}
+         </ModalComponent>
+         <main>
+             контент
+         </main>
+     </HomeLayout>
+    );
 }
---
+++ src/components/authLayout/AuthLayout.tsx
@@ -0,0 +1,10 @@
+
+interface FormLayoutProps {
+    children:React.ReactNode;
+}
+export const AuthLayout:React.FC<FormLayoutProps> = ({children}) => {
+    //protected Layout for checking isAuth
+    return (
+        <>{children}</>
+    );
+};
