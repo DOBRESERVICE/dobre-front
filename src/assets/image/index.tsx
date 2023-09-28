cf5a4eb5dcb8b556a671c915d095b2afaf0d9e11
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-09-28 10:41:27 +0300

    feature: create first item block

--- src/assets/image/index.tsx
+++ src/assets/image/index.tsx
@@ -1,17 +1,18 @@
-import acceptIcon from "./acceptIcon.svg";
-import appleIcon from "./apple.svg";
-import DOBRE from "./DOBRE.svg";
-import eyeClosed from "./eyeClosed.png";
-import eyeOpen from "./eyeOpen.png";
-import googleIcon from "./google.svg";
-import autorizBanner from "./leftBanner.png";
-import markerIcon from "./markerIcon.svg";
-import messageIcon from "./messageIcon.svg";
-import shopIcon from "./shopIcon.svg";
-import userIcon from "./userIcon.svg";
-import vkIcon from "./vk.svg";
-import whiteLogo from "./whiteLogo.svg";
-import yandexIcon from "./yandex.svg";
+import acceptIcon from './acceptIcon.svg';
+import appleIcon from './apple.svg';
+import DOBRE from './DOBRE.svg';
+import eyeClosed from './eyeClosed.png';
+import eyeOpen from './eyeOpen.png';
+import googleIcon from './google.svg';
+import autorizBanner from './leftBanner.png';
+import markerIcon from './markerIcon.svg';
+import messageIcon from './messageIcon.svg';
+import shopIcon from './shopIcon.svg';
+import userIcon from './userIcon.svg';
+import vkIcon from './vk.svg';
+import whiteLogo from './whiteLogo.svg';
+import yandexIcon from './yandex.svg';
+import plusIcon from './plusIcon.svg';
 
 export {
   acceptIcon,
@@ -28,4 +29,5 @@ export {
   markerIcon,
   messageIcon,
   shopIcon,
+  plusIcon,
 };
