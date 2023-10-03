3e4a7ff5e1d4ead7e2fab1570d1328d8472f0acd
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-10-03 14:49:00 +0300

    fix:urls

--- src/api/authApi/index.ts
+++ src/api/authApi/index.ts
@@ -1,15 +1,14 @@
-import axios from 'axios';
-import { loginUrl, registerUrl } from '@/constants/urls';
 import { AuthResponse, RegisterData } from '@/interfaces';
+import { defaultRequest } from '@/api/axios/instances';
 
 export const registerUser = async (email: string, password: string) => {
-  return await axios.post<RegisterData, AuthResponse>(registerUrl, {
+  return await defaultRequest.post<RegisterData, AuthResponse>('/auth/register', {
     email,
     password,
   });
 };
 export const loginUser = async (email: string, password: string) => {
-  return await axios.post<RegisterData, AuthResponse>(loginUrl, {
+  return await defaultRequest.post<RegisterData, AuthResponse>('/auth/login', {
     email,
     password,
   });
--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,1 @@
-export const registerUrl = 'http://ec2-3-120-174-49.eu-central-1.compute.amazonaws.com:7000/dev/auth/register';
-export const loginUrl = 'http://ec2-3-120-174-49.eu-central-1.compute.amazonaws.com:7000/dev/auth/login';
+export const BASE_URL = 'http://ec2-3-120-174-49.eu-central-1.compute.amazonaws.com:7000/dev';
