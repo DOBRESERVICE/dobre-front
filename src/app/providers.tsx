ff9e763dc38436876876d81cf574bd33de2376db
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-10-17 22:35:12 +0300

    refactor:delete unused data

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,8 +4,6 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { colors } from '@/constants/colors';
 import { Manrope } from 'next/font/google';
 import { ReactNode } from 'react';
-import { Provider } from 'react-redux';
-import { store } from '@/redux/store';
 
 const manrope = Manrope({
   style: ['normal'],
@@ -24,10 +22,8 @@ const theme = createTheme({
 });
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <AuthProvider>{children}</AuthProvider>;
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>{children}</AuthProvider>;
+    </ThemeProvider>
   );
 }
