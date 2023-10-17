3527a9845714db76f7396de0b4cfd16c50e66520
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-10-17 11:56:58 +0300

    fix:build error

--- src/components/LogIn/LogInComponent.tsx
+++ src/components/LogIn/LogInComponent.tsx
@@ -1,11 +1,9 @@
 'use client';
 import TextField from '@mui/material/TextField';
 import styles from './LogIn.module.scss';
-import Image from 'next/image';
 import { ChangeEvent, FC, useEffect, useState } from 'react';
 import { Button, Checkbox } from '@mui/material';
-import { eyeClosed, eyeOpen } from '@/assets/image';
-import { AuthStep, useAuthData } from '@/context/authContext';
+import { useAuthData } from '@/context/authContext';
 import { emailRules } from '@/constants';
 import { AuthServices } from '@/app/login/common/AuthServices/AuthServices';
 import { AuthHeader } from '@/app/login/common/AuthHeader/AuthHeader';
