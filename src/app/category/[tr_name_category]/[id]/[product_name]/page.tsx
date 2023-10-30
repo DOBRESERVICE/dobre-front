a813d089807cef1df9a74efeb8bef7d87e99d210
harmmselution <bugkfbdhdh@gmail.com>
2023-10-30 15:13:26 +0300

    fix:fix auth page and search block

---
+++ src/app/category/[tr_name_category]/[id]/[product_name]/page.tsx
@@ -0,0 +1,13 @@
+export default async function ProductPage({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { [key: string]: string };
+}) {
+  return (
+    <>
+      <p>asfasfasfasfasf</p>
+    </>
+  );
+}
--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://ec2-3-68-166-97.eu-central-1.compute.amazonaws.com:7000/dev';
+export const BASE_URL = 'http://ec2-3-126-106-58.eu-central-1.compute.amazonaws.com:7000/dev';
