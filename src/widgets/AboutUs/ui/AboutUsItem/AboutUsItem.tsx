e5d1933d0a16e3d54af4eb6acbf5f900b294e4c4
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-12-14 09:53:54 +0300

    fix:about us section

--- src/widgets/AboutUs/ui/AboutUsItem/AboutUsItem.tsx
+++ src/widgets/AboutUs/ui/AboutUsItem/AboutUsItem.tsx
@@ -14,10 +14,12 @@ type AboutItemType = {
 export const AboutUsItem: FC<AboutItemType> = ({ title, description, position }) => {
   return (
     <div className={styles.mainItem}>
+      <Image src={aboutUsBlob} alt='blob' className={styles[position]} />
       <div className={styles.itemTitle}>
-        {title}
-        <Image src={aboutUsLine} alt='line' className={styles.lineTitle} />
-        <Image src={aboutUsBlob} alt='blob' className={styles[position]} />
+        <div className={styles.titleContent}>
+          <p>{title}</p>
+          <Image src={aboutUsLine} alt='line' className={styles.lineTitle} />
+        </div>
       </div>
       <div className={styles.itemDescr}>{description}</div>
     </div>
